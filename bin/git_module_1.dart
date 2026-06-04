7426fd98a49f9840547bce12e0df1ac27e5951c2
Shamsul-Abir <samsulabir007@gmail.com>
2026-06-04 10:42:03 +0600

    updated

--- bin/git_module_1.dart
+++ bin/git_module_1.dart
@@ -1,2 +1,5 @@
-// command "git add ." - to push all folder in the githut
+// command "git add ." - to push all folder in the github
 //through terminal
+
+//if terminal shows "index file corrupt" than
+//run this in the terminal "Remove-Item .git\index"
