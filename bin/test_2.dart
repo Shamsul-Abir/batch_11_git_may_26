8e46b63b64bf91f22b3d0256bc8eb70dd02eb966
Shamsul-Abir <samsulabir007@gmail.com>
2026-06-04 16:10:07 +0600

    test

--- bin/test_2.dart
+++ bin/test_2.dart
@@ -1,6 +1,12 @@
 //git fetch - to update main branch
 // git pull - to get all files with the update
 
+//git reset bin/test_2.dart
+
+
 main(){
   print('Hello test 2');
+  print('Hello test 3');
+  print('Hello test 4');
+  print('Hello test 5');
 }
